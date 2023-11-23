a1325cdf7de4a2eceac755370c701311e9ed8844
Guillermo Avalos <guillermoavalosfloo@gmail.com>
2023-11-23 15:31:20 -0600

    edit controler

--- src/main/java/com/example/mercaweb/controller/ProductoController.java
+++ src/main/java/com/example/mercaweb/controller/ProductoController.java
@@ -1,10 +1,15 @@
 package com.example.mercaweb.controller;
 
+import com.example.mercaweb.domain.Pojo.ProductoPojo;
 import com.example.mercaweb.domain.Pojo.service.IProductoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,8 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductoController {
 
     private final IProductoService iProductoService;
+@GetMapping
+    public ResponseEntity<List<ProductoPojo>> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(iProductoService.getAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<ProductoPojo> getProducto(@PathVariable Integer id){
+    return ResponseEntity.of(iProductoService.getProducto(id));
 
 
+    }
 
 
 
