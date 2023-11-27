ec2e43ab422b1abdb9c55beccf1254be1903fc11
Guillermo Avalos <guillermoavalosfloo@gmail.com>
2023-11-27 14:26:29 -0600

    Edit Controller save and edit

--- src/main/java/com/example/mercaweb/controller/ProductoController.java
+++ src/main/java/com/example/mercaweb/controller/ProductoController.java
@@ -5,10 +5,8 @@ import com.example.mercaweb.domain.Pojo.service.IProductoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -33,6 +31,18 @@ public class ProductoController {
     return ResponseEntity.of(iProductoService.getProducto(id));
      */
     }
+
+    public ResponseEntity<ProductoPojo> save(@RequestBody  ProductoPojo prodcutoNew){
+
+        try{
+            return ResponseEntity.status(HttpStatus.CREATED).body(iProductoService.save(prodcutoNew));
+
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+    }
+
 
 
 
